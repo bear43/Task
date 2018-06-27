b510bc38b6a94e3d411be0e190f31ea326b59676
novice43 <bigwilly4307@gmail.com>
2018-06-28 00:07:45 +0400

    First ten tasks are ready.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        t9.run();
+        t10.run();
     }
 }
--- src/t10.java
+++ src/t10.java
@@ -7,11 +7,18 @@ class t10
         File file = new File();
         int verticesCount = Integer.parseInt(file.getReader().readLine());
         int[][] table = file.readMatrix(verticesCount, verticesCount);
+        file.getReader().close();
         for(int i = 0; i < verticesCount; i++)
             for(int j = 0; j < verticesCount; j++)
                 if(table[i][j] == -1)
                     table[i][j] = Integer.MAX_VALUE;
         t9.makeFloyd(table, verticesCount);
+        int max = Integer.MIN_VALUE;
+        for (int[] i : table)
+            for(int j : i)
+                if(j > max)
+                    max = j;
+        file.getWriter().println(max);
         file.getWriter().flush();
         file.getWriter().close();
     }
